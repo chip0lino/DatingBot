cb80ff56e75a6ce141b5078db7aa213200eacee5
Unrealxo <123935394+Unrealxo@users.noreply.github.com>
2024-02-08 18:08:36 +0300

    database.py

--- dating telegram bot/database/database.py
+++ dating telegram bot/database/database.py
@@ -2,17 +2,22 @@ import sqlite3
 
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS users ("
-                   "user_id INTEGER PRIMARY KEY, "
-                   "photo TEXT, " # айди фотки юзера
-                   "name TEXT, "
-                   "gender TEXT, "
-                   "age INTEGER, "
-                   "anketa_description TEXT, "
-                   "zodiac_sign TEXT, "
-                   "kurs INTEGER, "
-                   "faculty TEXT, "
-                   "grade INTEGER)") # кого хочет смотреть юзер
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        nickname TEXT,
+        name TEXT,
+        gender TEXT,
+        age INTEGER,
+        photo TEXT,
+        anketa_description TEXT,
+        zodiac_sign TEXT,
+        kurs INTEGER,
+        faculty TEXT,
+        grade INTEGER
+    )
+''')
 
 cursor.execute("CREATE TABLE IF NOT EXISTS like ("
                    "user_id INTEGER PRIMARY KEY, "
